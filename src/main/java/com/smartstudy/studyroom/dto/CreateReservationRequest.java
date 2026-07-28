546240a9fab14d714863c4192b82ce9de2dcd8a7
tianhaoxuan <279440584+tianhaoxuan@users.noreply.github.com>
2026-07-29 00:40:20 +0800

    build: upgrade to Java 17 and Spring Boot 3

--- src/main/java/com/smartstudy/studyroom/dto/CreateReservationRequest.java
+++ src/main/java/com/smartstudy/studyroom/dto/CreateReservationRequest.java
@@ -2,8 +2,8 @@ package com.smartstudy.studyroom.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
